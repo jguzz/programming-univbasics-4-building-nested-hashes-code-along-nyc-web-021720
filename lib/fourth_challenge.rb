883799435bb51361e537b01262ecf0fbec51af6e
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-02 23:52:48 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,14 +23,14 @@ def fourth_challenge
       :heroine_friends => [
         #your hashes here!
         {
-          :name
-          :age
-          :attitude
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
         },{
-          :name
-          :age
-          :attitude
-        }
+          :name => "Nurse",
+          :age => "44",
+          :attitude =>  "worried"
+          }
       ]
    }
 }
